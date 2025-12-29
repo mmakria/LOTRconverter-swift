2dbb2f2aa1b723f634e58eb1e87ee8ff5471cadb
momomak <m.makria@gmail.com>
2025-12-29 18:07:10 +0100

    Initial Commit

---
+++ LOTRconverter/LOTRconverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRconverterApp.swift
+//  LOTRconverter
+//
+//  Created by Mohamed Makria on 29/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct LOTRconverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
