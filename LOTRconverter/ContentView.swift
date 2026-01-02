0025d9abc57663461d415820311333d48e463782
momomak <m.makria@gmail.com>
2026-01-02 09:41:57 +0100

    Adding textInputs + modifiers

--- LOTRconverter/ContentView.swift
+++ LOTRconverter/ContentView.swift
@@ -8,12 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var showExchangeInfo = false
+    @State var leftAmount = ""
+    @State var rightAmount = ""
+    
+    
     var body: some View {
         ZStack{
             //Background image
+            // Remplace Image(.background) par ceci :
+           
+            Image(.background)
+                .resizable()
+                .ignoresSafeArea()
+           
+            
             VStack{
                 //pony image
+                Image(.prancingpony)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
+                
                 //Currency exchange text
+                Text("Currency exchange")
+                    .font(Font.largeTitle.bold())
+                    .foregroundStyle(.white)
+                
                 //conversion section
                 HStack{
                     //left conversion section
@@ -21,29 +43,74 @@ struct ContentView: View {
                         //currency
                         HStack{
                             //currency image
+                            Image(.silverpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
                             
                             //currency text
-                            
+                            Text("Silver piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
                         }
+                        .padding(.bottom, -5)
                         //text field
                         
+                        TextField("Amount", text: $leftAmount)
+                            .textFieldStyle(.roundedBorder)
+                        
+                        
                     }
-                    //equal sign
+                    //equal sign Sf symbols
+                    Image(systemName: "equal")
+                        .font(Font.largeTitle)
+                        .foregroundStyle(.white)
+                        .symbolEffect(.pulse)
+                    
+                    
                     //right conversion section
                     VStack{
                         //currency
                         HStack{
-                            //currency image
-                            
                             //currency text
+                            Text("Glod Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
+                            
+                            //currency image
+                            Image(.goldpiece)
+                                .resizable( )
+                                .scaledToFit( )
+                                .frame(height: 33)
                             
                         }
+                        .padding(.bottom, -5)
                         //text field
-                        
+                        TextField("Amount", text: $rightAmount)
+                            .textFieldStyle(.roundedBorder)
+                            .multilineTextAlignment(.trailing)
                     }
                 }
-                //buton
+                .padding()
+                .background(.black.opacity(0.5))
+                .clipShape(.capsule)
+                Spacer()
+                //Info buton
+                
+                HStack {
+                    Spacer()
+                    Button{
+                        showExchangeInfo.toggle()
+                    } label: {
+                        Image(systemName: "info.circle.fill")
+                            .font(Font.largeTitle)
+                            .foregroundStyle(.white)
+                    }.padding(.trailing)
+                }
+                
+              
             }
+//            .border(.blue)
         }
     }
 }
